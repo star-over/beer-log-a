eafc8526f6f08b52fdaef99274a82b188e1c123f
FFire <8497715@gmail.com>
2023-02-01 00:17:12 +0500

    fix small fixes

--- src/components/HeartButton.jsx
+++ src/components/HeartButton.jsx
@@ -6,8 +6,8 @@ export function HeartButton({ checked, onChange }) {
   return (
     <label>
       <input className="peer hidden" type="checkbox" checked={checked} onChange={onChange} />
-      <IconOutLine className="peer-checked:hidden  text-gray-400 hover:text-gray-500" aria-hidden="true" />
-      <IconSolid className="hidden peer-checked:block   text-red-400 hover:text-red-500" aria-hidden="true" />
+      <IconOutLine className="peer-checked:hidden  text-gray-400 hover:text-gray-500 cursor-pointer" aria-hidden="true" />
+      <IconSolid className="hidden peer-checked:block   text-red-400 hover:text-red-500 cursor-pointer" aria-hidden="true" />
     </label>
   );
 }
